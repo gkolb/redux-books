cc397e363934eb783f83de853e0e2c6c0e6c10b9
gkolb <gkolb100@gmail.com>
2018-04-25 19:48:02 -0600

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+function () {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry Potter 1'},
+        {title: 'Harry Potter 2'},
+        {title: 'Harry Potter 3'}
+    ]
+}
